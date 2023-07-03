fdc3283fff7b38f06f70bff536f66d0f5acb4460
Olga Meleshko <omeleshko@dalet.com>
2023-07-04 02:42:16 +0300

    Add TS.

--- src/Service/index.ts
+++ src/Service/index.ts
@@ -1,32 +1,41 @@
-import { createServer } from 'http';
+import {createServer, IncomingMessage, ServerResponse} from 'http';
 import * as url from 'url';
-import { fileURLToPath } from 'url';
-import { readFile } from 'fs/promises';
-import { dirname } from 'path';
-import { v4 as uuid, validate } from 'uuid';
-import { createWriteStream } from 'fs';
+import {readFile} from 'fs/promises';
+import {v4 as uuid, validate} from 'uuid';
+import {createWriteStream} from 'fs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+interface User {
+    id: string;
+    username: string;
+    age: number;
+    hobbies: string[];
+}
+
+enum HttpMethod {
+    POST = 'POST',
+    GET = 'GET',
+    PUT = 'PUT',
+    DELETE = 'DELETE'
+}
+
 
 const baseApi = '/api/users';
 
-const getUsers = async () => {
+const getUsers = async (): Promise<User[]> => {
     try {
         const filePath = getPath();
-        const contents = await readFile(filePath, { encoding: 'utf8' });
-        console.log(contents)
-        return JSON.parse(contents || []);
+        const contents = await readFile(filePath, {encoding: 'utf8'});
+        return JSON.parse(contents || '');
     } catch (e) {
         throw e;
     }
 }
 
 const getPath = () => {
-    return `${__dirname}/database/users.json`;
+    return `${__dirname}/users.json`;
 }
 
-const deleteUser = async (uuid) => {
+const deleteUser = async (uuid: string) => {
     try {
         const users = await getUsers();
         console.log('DELETE ', users)
@@ -34,8 +43,6 @@ const deleteUser = async (uuid) => {
             throw new Error('404');
         }
         const updated = users.filter(u => u.id !== uuid);
-        console.log(updated);
-
         updateFileContent(updated);
     } catch (e) {
         console.log(e);
@@ -43,27 +50,23 @@ const deleteUser = async (uuid) => {
     }
 }
 
-const updateFileContent = (content) => {
+const updateFileContent = (content: User[]) => {
     const writeStream = createWriteStream(getPath());
     writeStream.write(JSON.stringify(content));
 }
 
-const getUser = async (uuid) => {
+const getUser = async (uuid: string): Promise<User | undefined> => {
     const users = await getUsers();
     return users.find(user => user.id === uuid);
 }
 
 
-const createUser = async (user) => {
+const createUser = async (user: Omit<User, 'id'>) => {
     try {
         const users = await getUsers();
-        console.log(users)
         const updated = [...(users || []), {
-            ...user,
-            id: uuid()
+            ...user, id: uuid()
         }];
-
-        console.log(updated);
 
         updateFileContent(updated);
 
@@ -73,7 +76,7 @@ const createUser = async (user) => {
     }
 }
 
-const updateUser = async (uuid, data) => {
+const updateUser = async (uuid: string, data: Partial<Omit<User, 'id'>>) => {
     try {
         const users = await getUsers();
         const user = users.find(u => u.id === uuid);
@@ -81,8 +84,7 @@ const updateUser = async (uuid, data) => {
             throw new Error('404');
         }
         const updated = [...(users || []), {
-            ...user,
-            ...data
+            ...user, ...data
         }];
 
         console.log(updated);
@@ -95,35 +97,34 @@ const updateUser = async (uuid, data) => {
     }
 }
 
-const validateCreateBody = (userData) => {
-    return !!(!userData.username && !!userData.age && Array.isArray(userData.hobbies) && validateUpdateBody());
+const validateCreateBody = (userData: User) => {
+    return (!!userData.username && !!userData.age && Array.isArray(userData.hobbies) && validateUpdateBody(userData));
 }
 
-const validateUpdateBody = (userData) => {
+const validateUpdateBody = (userData: Partial<User>) => {
     const isNameValid = userData.username ? typeof userData.username === 'string' : true;
     const isAgeValid = userData.age ? typeof userData.age === 'number' : true;
-    const isHobbyValid =  (!userData.hobbies || userData.hobbies?.length === 0)
-        ? true : userData.hobbies?.every(hobby => typeof hobby === 'string');
+    const isHobbyValid = (!userData.hobbies || userData.hobbies?.length === 0) ? true : userData.hobbies?.every((hobby: string) => typeof hobby === 'string');
 
-    return !!(isNameValid && isAgeValid && isHobbyValid);
+    return (isNameValid && isAgeValid && isHobbyValid);
 }
 
-const isReqById = (pathname) => {
+const isReqById = (pathname = ''): boolean => {
     const splitted = pathname.split('/');
     return (splitted.slice(0, splitted.length - 1).join('/') === baseApi);
 }
 
-const getPathname = (req) => {
-    return url.parse(req.url, true).pathname;
+const getPathname = (req: IncomingMessage): string => {
+    return url.parse(req.url || '').pathname || '';
 }
 
-const getUuid = (pathname) => {
+const getUuid = (pathname: string): string => {
     const splitted = pathname.split('/');
 
-    return splitted[splitted.length - 1]
+    return splitted[splitted.length - 1] || '';
 }
 
-const get = async (req, res) => {
+const get = async (req: IncomingMessage, res: ServerResponse) => {
     const pathname = getPathname(req);
 
     if (pathname === baseApi) {
@@ -145,9 +146,9 @@ const get = async (req, res) => {
                 res.writeHead(400);
                 return res.end();
             }
-            const user = getUser(uuid);
+            const user = await getUser(uuid);
             if (user) {
-                res.writeHead(200, { 'Content-type': 'text/plain' });
+                res.writeHead(200, {'Content-type': 'text/plain'});
                 res.write(JSON.stringify(user));
             } else {
                 res.writeHead(404);
@@ -166,11 +167,12 @@ const get = async (req, res) => {
     return res.end();
 }
 
-const post = async (req, res) => {
+const post = async (req: IncomingMessage,
+                    res: ServerResponse) => {
     const pathname = getPathname(req);
 
     if (pathname === baseApi) {
-        const chunks = [];
+        const chunks: Buffer[] = [];
         req.on('data', (chunk) => {
             chunks.push(chunk);
         });
@@ -194,13 +196,14 @@ const post = async (req, res) => {
                 return res.end();
             }
         });
+    } else {
+        res.writeHead(404);
+        return res.end();
     }
-
-    res.writeHead(404);
-    return res.end();
 }
 
-const put = async (req, res) => {
+const put = async (req: IncomingMessage,
+                   res: ServerResponse) => {
     const pathname = getPathname(req);
 
     if (isReqById(pathname)) {
@@ -210,7 +213,7 @@ const put = async (req, res) => {
             return res.end();
         }
 
-        const chunks = [];
+        const chunks: Buffer[] = [];
         req.on('data', (chunk) => {
             chunks.push(chunk);
         });
@@ -235,10 +238,14 @@ const put = async (req, res) => {
             }
         });
 
+    } else {
+        res.writeHead(404);
+        return res.end();
     }
 }
 
-const deleteMethod = async (req, res) => {
+const deleteMethod = async (req: IncomingMessage,
+                            res: ServerResponse) => {
     const pathname = getPathname(req);
 
     if (isReqById(pathname)) {
@@ -252,7 +259,7 @@ const deleteMethod = async (req, res) => {
             await deleteUser(uuid);
             res.writeHead(201);
             return res.end();
-        } catch (e) {
+        } catch (e: any) {
             if (e.message === '404') {
                 res.writeHead(404);
                 return res.end();
@@ -265,31 +272,34 @@ const deleteMethod = async (req, res) => {
     }
 }
 
-const requestListener = async function (req, res) {
+const requestListener = async function (
+    req: IncomingMessage,
+    res: ServerResponse
+) {
 
-    if (req.method === 'GET') {
+    if (req.method === HttpMethod.GET) {
         await get(req, res);
         return;
     }
 
-    if (req.method === 'POST') {
+    if (req.method === HttpMethod.POST) {
         await post(req, res);
         return;
     }
 
-    if (req.method === 'PUT') {
-        put(req, res);
+    if (req.method === HttpMethod.PUT) {
+        await put(req, res);
         return;
     }
 
-    if (req.method === 'DELETE') {
+    if (req.method === HttpMethod.DELETE) {
         await deleteMethod(req, res);
         return;
     }
 
 
-    res.writeHead(200);
-    res.end('Hello World from Node.js HTTP Server');
+    res.writeHead(500);
+    res.end();
 }
 
 const server = createServer(requestListener);
